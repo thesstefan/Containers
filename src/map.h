882d9fedd79ea29f11daaa59d1635f64876b8a9b
TheMightyStefan <stefanthefineman@gmail.com>
2018-02-10 16:59:38 +0200

    Add the most recent implementations

--- src/map.h
+++ src/map.h
@@ -1,79 +1,32 @@
-#include "vector.h"
-#include "pair.h"
+#pragma once
 
-template <typename Key, typename Value>
+#include "pair.h"
+#include "key_tree.h"
+#include "misc.h"
+
+template <typename KeyType, typename ValueType, typename Compare = NodeCompare<KeyType>>
 class Map {
-    protected:
-        Vector<Key> *keys;
-        Vector<Value> *vals;
+    typedef typename KeyTree<KeyType, ValueType, Compare>::Iterator Iterator;
+    typedef typename KeyTree<KeyType, ValueType, Compare>::ConstIterator ConstIterator;
+
+    private:
+        KeyTree<KeyType, ValueType, Compare> data;
 
     public:
-        class Iterator {
-            friend class Map;
-
-            protected:
-                typename Vector<Key>::Iterator key_it;
-                typename Vector<Value>::Iterator val_it;
-
-            public:
-                Iterator();
-                Iterator(typename Vector<Key>::Iterator key_it, typename Vector<Value>::Iterator val_it);
-                Iterator(const Iterator &iterator);
-
-                Iterator &operator=(const Iterator &iterator);
-
-                Iterator operator++();
-                Iterator operator++(int);
-
-                Iterator operator--();
-                Iterator operator--(int x);
-
-                Pair<Key, Value> &operator*();
-                Pair<Key, Value> *operator->();
-
-                bool operator==(const Iterator &iterator) const;
-                bool operator!=(const Iterator &iterator) const;
-        };
-
-        class ConstIterator {
-            friend class Map;
-
-            protected:
-                typename Vector<Key>::ConstIterator key_it;
-                typename Vector<Value>::ConstIterator val_it;
-
-            public:
-                ConstIterator();
-                ConstIterator(typename Vector<Key>::ConstIterator key_it, typename Vector<Value>::ConstIterator val_it);
-                ConstIterator(const ConstIterator &iterator);
-
-                ConstIterator &operator=(const ConstIterator &iterator);
-
-                ConstIterator operator++();
-                ConstIterator operator++(int);
-
-                ConstIterator operator--();
-                ConstIterator operator--(int);
-
-                const Pair<Key, Value> &operator*() const;
-                const Pair<Key, Value> *operator->() const;
-
-                bool operator==(const ConstIterator &iterator) const;
-                bool operator!=(const ConstIterator &iterator) const;
-        };
-
         Map();
+        Map(const Map& other);
+
+        template <typename IteratorType>
+        Map(IteratorType first, IteratorType last);
+
         ~Map();
 
-        Map& operator=(const Map &other);
+        Map& operator=(const Map &other); 
 
-        Value& at(const Key &key);
-        const Value& at(const Key &key) const;
+        ValueType& at(const KeyType& key);
+        const ValueType& at(const KeyType& key) const;
 
-        Value& operator[](const Key &key);
-
-        bool empty() const;
-        unsigned int size() const;
+        ValueType& operator[](const KeyType& key);
 
         Iterator begin();
         Iterator end();
@@ -81,22 +34,282 @@ class Map {
         ConstIterator cbegin() const;
         ConstIterator cend() const;
 
+        bool empty() const;
+        size_t size() const;
+
         void clear();
 
-        Pair<Iterator, bool> insert(const Key &key, const Value &val);
+        Iterator insert(const Pair<KeyType, ValueType>& pair);
+        Iterator insert(const KeyType& key, const ValueType& value);
 
-        /*
+        template <typename IteratorType>
+        void insert(IteratorType first, IteratorType last);
+
         void erase(Iterator position);
-        unsigned int erase(const Key &key);
-        */
+        size_t erase(const KeyType& key);
 
-        ConstIterator find(const Key &key) const;
-        Iterator find(const Key &key);
+        template <typename IteratorType>
+        void erase(IteratorType first, IteratorType last);
 
-        unsigned int count(const Key &key) const;
-        //Iterator erase(ConstIterator position);
-//        void erase(Iterator position);
+        void swap(Map<KeyType, ValueType, Compare>& other);
 
-//        unsigned int count(const Key &key) const;
-//        Iterator find(const Key &key);
+        size_t count (const KeyType& key) const;
+
+        Iterator find(const KeyType& key);
+        ConstIterator find(const KeyType& key) const;
+
+        Iterator lower_bound(const KeyType& key);
+        ConstIterator lower_bound(const KeyType& key) const;
+
+        Iterator upper_bound(const KeyType &key);
+        ConstIterator upper_bound(const KeyType& key) const;
+
+        Pair<Iterator, Iterator> equal_range(const KeyType& key);
+        Pair<ConstIterator, ConstIterator> equal_range(const KeyType& key) const;
+
+        bool operator==(const Map& other) const;
+        bool operator!=(const Map& other) const;
+
+        bool operator<(const Map& other) const;
+        bool operator>(const Map& other) const;
+        bool operator<=(const Map& other) const;
+        bool operator>=(const Map& other) const;
 };
+
+template <typename KeyType, typename ValueType, typename Compare>
+Map<KeyType, ValueType, Compare>::Map() {
+    this->data = KeyTree<KeyType, ValueType>();
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+template <typename IteratorType>
+Map<KeyType, ValueType, Compare>::Map(IteratorType first, IteratorType last) {
+    this->data = KeyTree<KeyType, ValueType>();
+
+    for (auto it = first; it != last; it++)
+        this->data.insert(*it);
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+Map<KeyType, ValueType, Compare>::Map(const Map& other) {
+    this->data = other.data;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+Map<KeyType, ValueType, Compare>::~Map() {}
+
+template <typename KeyType, typename ValueType, typename Compare>
+Map<KeyType, ValueType, Compare>& Map<KeyType, ValueType, Compare>::operator=(const Map& other) {
+    this->data = other.data;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+ValueType& Map<KeyType, ValueType, Compare>::at(const KeyType& key) {
+    auto result = this->data.search(key);
+
+    if (result == nullptr)
+        throw std::out_of_range("KeyType is not present in Map (Map::at(const KeyType& key))");
+
+    return result->value;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+const ValueType& Map<KeyType, ValueType, Compare>::at(const KeyType& key) const {
+    auto result = this->data.search(key);
+
+    if (result == nullptr)
+        throw std::out_of_range("KeyType is not present in Map (Map::at(const KeyType& key))");
+
+    return result->value;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+ValueType& Map<KeyType, ValueType, Compare>::operator[](const KeyType& key) {
+    auto find = this->data.search(key);
+
+    if (find == nullptr)
+        return this->insert(key, ValueType())->value;
+
+    return find->value;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::Iterator Map<KeyType, ValueType, Compare>::begin() {
+    return this->data.begin();
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::Iterator Map<KeyType, ValueType, Compare>::end() {
+    return this->data.end();
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::ConstIterator Map<KeyType, ValueType, Compare>::cbegin() const {
+    return this->data.cbegin();
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::ConstIterator Map<KeyType, ValueType, Compare>::cend() const {
+    return this->data.cend();
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+bool Map<KeyType, ValueType, Compare>::empty() const {
+    return this->data.cbegin() == this->data.cend();
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+size_t Map<KeyType, ValueType, Compare>::size() const {
+    return distance(this->data.cbegin(), this->data.cend());
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+void Map<KeyType, ValueType, Compare>::clear() {
+    this->erase(this->begin(), this->end());
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::Iterator Map<KeyType, ValueType, Compare>::insert(const Pair<KeyType, ValueType>& pair) {
+    this->data.insert(pair.first, pair.second);
+
+    return Map<KeyType, ValueType, Compare>::Iterator(this->data.search(pair.first), this->data.max());
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::Iterator Map<KeyType, ValueType, Compare>::insert(const KeyType& key, const ValueType& value) {
+    this->data.insert(key, value);
+
+    return Map<KeyType, ValueType, Compare>::Iterator(this->data.search(key), this->data.max());
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+template <typename IteratorType>
+void Map<KeyType, ValueType, Compare>::insert(IteratorType first, IteratorType last) {
+    for (auto it = first; it != last; it++)
+        this->data.insert(it->key, it->value);
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+void Map<KeyType, ValueType, Compare>::erase(typename Map<KeyType, ValueType, Compare>::Iterator position) {
+    this->data.erase(position->key);
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+template <typename IteratorType>
+void Map<KeyType, ValueType, Compare>::erase(IteratorType first, IteratorType last) {
+    for (auto it = first; it != last; it++)
+        this->data.erase(it->key);
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+size_t Map<KeyType, ValueType, Compare>::erase(const KeyType& key) {
+    size_t count = 0;
+
+    while (this->data.search(key) != nullptr) {
+        this->data.erase(key);
+
+        count++;
+    }
+
+    return count;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+void Map<KeyType, ValueType, Compare>::swap(Map<KeyType, ValueType, Compare>& other) {
+    auto temp = this->data;
+
+    this->data = other.data;
+
+    other.data = temp;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+size_t Map<KeyType, ValueType, Compare>::count(const KeyType& key) const {
+    return (this->data.search(key) != nullptr) ? 1 : 0;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::Iterator Map<KeyType, ValueType, Compare>::find(const KeyType& key) {
+    return Map<KeyType, ValueType, Compare>::Iterator(this->data.search(key), this->data.max());
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::ConstIterator Map<KeyType, ValueType, Compare>::find(const KeyType& key) const {
+    return Map<KeyType, ValueType, Compare>::ConstIterator(this->data.search(key), this->data.max());
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::Iterator Map<KeyType, ValueType, Compare>::lower_bound(const KeyType& key) {
+    auto it = this->begin();
+
+    for ( ; it != this->end() && !(key < it->key); it++);
+
+    return it;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::ConstIterator Map<KeyType, ValueType, Compare>::lower_bound(const KeyType& key) const {
+    auto it = this->cbegin();
+
+    for ( ; it != this->cend() && !(key < it->key); it++);
+
+    return it;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::Iterator Map<KeyType, ValueType, Compare>::upper_bound(const KeyType& key) {
+    auto it = this->begin();
+
+    for ( ; it != this->end() && key > it->key ; it++);
+
+    return it;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+typename Map<KeyType, ValueType, Compare>::ConstIterator Map<KeyType, ValueType, Compare>::upper_bound(const KeyType& key) const {
+    auto it = this->cbegin();
+
+    for ( ; it != this->cend() && key > it->key ; it++);
+
+    return it;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+Pair<typename Map<KeyType, ValueType, Compare>::Iterator, typename Map<KeyType, ValueType, Compare>::Iterator> Map<KeyType, ValueType, Compare>::equal_range(const KeyType& key) {
+    return Pair<typename Map<KeyType, ValueType, Compare>::Iterator, typename Map<KeyType, ValueType, Compare>::Iterator>(this->lower_bound(key), this->upper_bound(key));
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+Pair<typename Map<KeyType, ValueType, Compare>::ConstIterator, typename Map<KeyType, ValueType, Compare>::ConstIterator> Map<KeyType, ValueType, Compare>::equal_range(const KeyType& key) const {
+    return Pair<typename Map<KeyType, ValueType, Compare>::ConstIterator, typename Map<KeyType, ValueType, Compare>::ConstIterator>(this->lower_bound(key), this->upper_bound(key));
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+bool Map<KeyType, ValueType, Compare>::operator==(const Map<KeyType, ValueType, Compare>& other) const {
+    return this->data == other.data;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+bool Map<KeyType, ValueType, Compare>::operator!=(const Map<KeyType, ValueType, Compare>& other) const {
+    return this->data != other.data;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+bool Map<KeyType, ValueType, Compare>::operator<(const Map<KeyType, ValueType, Compare>& other) const {
+    return this->data < other.data;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+bool Map<KeyType, ValueType, Compare>::operator>(const Map<KeyType, ValueType, Compare>& other) const {
+    return this->data > other.data;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+bool Map<KeyType, ValueType, Compare>::operator<=(const Map<KeyType, ValueType, Compare>& other) const {
+    return this->data < other.data;
+}
+
+template <typename KeyType, typename ValueType, typename Compare>
+bool Map<KeyType, ValueType, Compare>::operator>=(const Map<KeyType, ValueType, Compare>& other) const {
+    return this->data < other.data;
+}
--- src/stack.h
+++ src/stack.h
@@ -1,20 +1,62 @@
+/** 
+ * @file stack.h
+ *
+ * This module provides an implementation of a Stack 
+ */
+
 #pragma once
 
 #include "vector.h"
 
+/**
+ * @tparam ItemType the type of item the stack will contain
+ */
 template <typename ItemType, typename ContainerType = Vector<ItemType>>
+/** 
+ * @class Stack
+ *
+ * @brief A class implementing a Stack-type container
+ */
 class Stack {
     private:
+        /** @brief The underlying container of the Stack */
         ContainerType container;
 
     public:
+        /** @brief Default constructor */
         Stack();
+
+        /** 
+         * @brief Copy constructor
+         *
+         * The underlying container is copy-constructed with the content of @b other.container
+         */
         Stack(const Stack& other);
 
+        /**
+         * @brief Default destructor
+         *
+         * Destructs the underlying container
+         */
         ~Stack();
 
+        /**
+         * @brief Assignment operator
+         *
+         * @details Replaces the content of the underlying container with those of @b other
+         */
         Stack& operator=(const Stack<ItemType, ContainerType>& other);
 
+        /**
+         * @brief Checks if the underlying container has no elements
+         *
+         * Calls @b container.empty()
+         *
+         * @return 
+         *      true if the Stack is empty
+         *
+         *      false if the Stack is not empty
+         */ 
         bool empty() const;
         size_t size() const;
 
